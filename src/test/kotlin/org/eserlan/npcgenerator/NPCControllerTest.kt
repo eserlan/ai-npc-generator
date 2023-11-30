71bead607eb8878674305edcc7c24309eecaabc3
Espen <espen.erlandsen@gmail.com>
2023-11-30 14:45:18 +0100

    added chad, for injection

--- src/test/kotlin/org/eserlan/npcgenerator/NPCControllerTest.kt
+++ src/test/kotlin/org/eserlan/npcgenerator/NPCControllerTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -12,6 +13,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 @WebMvcTest
 class NPCControllerTest (@Autowired val mockMvc: MockMvc){
 
+    @MockBean
+    lateinit var chad: ChatGPTService
     @Test
     fun helloKotlin() {
         mockMvc.perform(get("/hello")
